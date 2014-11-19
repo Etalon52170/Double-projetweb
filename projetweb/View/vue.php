d0be1a22f7885c005eaf68aa93c5c51ae2b0b110
sopretty <rpzle54230@hotmailfr>
2014-11-19 15:39:08 +0100

    Modification de la vue

--- projetweb/View/vue.php
+++ projetweb/View/vue.php
@@ -1,4 +1,5 @@
 <?php
+
 class Vue {
 
     protected $tab;
@@ -31,16 +32,16 @@ class Vue {
     private function acceuilpage() {
         $res = '
         <header>
-            <div id=\'inscription\'>
-                <a href = \'blog.php?a=inscription\'>Inscription</a>
+            <div class = \'right-co\'>
+                <button class="btn btn-sm btn-default" type="button">Inscription</button>
             </div>
-            <form id=\'login\' action=\'auth.php\' method=\'post\'>
-                <div classboutton\'>
-                    <button type=\'submit\'>Connexion</button>
-                </div>
-                <div id= identification >
+            <div class=\'right-co\'>
+                <form id=\'login\' action=\'auth.php\' method=\'post\'>
                     <input type=\'text\' name=\'nom\' placeholder= \'Nom\' required/>
-		<input type=\'password\' name=\'pwd\' placeholder= \'Mot de passe\' required/>
+                    <input type=\'password\' name=\'pwd\' placeholder= \'Mot de passe\' required/>
+                    <button class="btn btn-sm btn-default" type="button">Connexion</button>
+                </form>
+            </div>       </header>
                 ';
         return $res;
     }
@@ -49,11 +50,14 @@ class Vue {
         $res = '<!DOCTYPE html>
 <html lang="fr">
 
-<head>
-	<title>Affichage général</title>
-	<meta charset="utf-8" />
-	<link rel="stylesheet" href="style.css" type="text/css" /> 
-</head>  
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel=\'stylesheet\' href=\'prototype\CascadeStyleSheet.css\' media=\'all\' type=\'text/css\' />
+         <!-- Bootstrap -->
+         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    </head>
   
 <body>';
 
@@ -66,7 +70,7 @@ class Vue {
             $res .= $this->defaut();
         }
 
-        $res = $res . '</body></html>';
+        $res = $res . '<script src="../bootstrap/js/bootstrap.min.js"></script></body></html>';
         return $res;
     }
 
