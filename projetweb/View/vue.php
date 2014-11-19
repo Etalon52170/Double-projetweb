a2af0a4c66ef9ee48bc1980e4d069da3340dc4dc
nataris88 <nataris88@live.fr>
2014-11-19 20:49:13 +0100

    test d'affichage pour les stats des joueurs + ajout image pour les test dans prototype

--- projetweb/View/vue.php
+++ projetweb/View/vue.php
@@ -54,7 +54,7 @@ class Vue {
         <title>TODO supply a title</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel=\'stylesheet\' href=\'prototype\CascadeStyleSheet.css\' media=\'all\' type=\'text/css\' />
+        <link rel=\'stylesheet\' href=\'prototype/CascadeStyleSheet.css\' media=\'all\' type=\'text/css\' />
          <!-- Bootstrap -->
          <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
     </head>
