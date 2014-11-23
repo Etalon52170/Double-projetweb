1c8033f037c02067923bd4206c8eddb64124feda
sopretty <rpzle54230@hotmailfr>
2014-11-23 21:57:25 +0100

    Ajout de la requete ajax pour l'inscription ainsi que des tests pour l'inscription et la méthode de retour dans "inscription"

--- projetweb/JS/Js_Connexion.js
+++ projetweb/JS/Js_Connexion.js
@@ -1,13 +1,14 @@
 
 function validerConnexion() {
-    var acc = document.getElementById("log").value;
-    var pass = document.getElementById("pwd").value;
+
+    var acc = document.getElementById("login").value;
+    var pass = document.getElementById("password").value;
     XmlHttp = GetXmlHttpObject();
     if (XmlHttp == null) {
         alert("Objets HTTP non supportés");
     } else {
         XmlHttp.onreadystatechange = stateChanged;
-        XmlHttp.open("GET", "./Controller/ReponsesAjax.php?pwd=" + pass + "&log=" + acc, false);
+        XmlHttp.open("GET", "./Controller/ReponsesAjax.php?action=conn&pwd=" + pass + "&log=" + acc, false);
         XmlHttp.send(null);
     }
 
@@ -29,14 +30,64 @@ function GetXmlHttpObject()
 
 function stateChanged()
 {
-    //var json;
     if (XmlHttp.readyState == 4) {
         json = JSON.parse(XmlHttp.responseText);
         if (json.find == "ok") {
-            window.location.href = "http://localhost/PhpProject1/Double-projetweb/projetweb/";
+            window.location.href = "http://localhost/PhpProject1/Double-projetweb/projetweb/index.php?a=jeux";
         } else {
             document.getElementById('mdpIncorrect').style.display = "block";
-            setTimeout(function() { document.getElementById('mdpIncorrect').style.display = "none"; },3000);
+            setTimeout(function() {
+                document.getElementById('mdpIncorrect').style.display = "none";
+            }, 3000);
+        }
+    }
+}
+
+function versInscription() {
+    location.href = "http://localhost/PhpProject1/Double-projetweb/projetweb/index.php?a=inscri";
+}
+
+function retour() {
+    location.href = "http://localhost/PhpProject1/Double-projetweb/projetweb/index.php";
+}
+
+function inscription() {
+    var acc = document.getElementById("log").value;
+    var pass = document.getElementById("pwd").value;
+    var email = document.getElementById("email").value;
+    if (acc == "" || pass == "" || email == "" || pass.length < 6) {
+        document.getElementById('messageError').style.display = "block";
+        setTimeout(function() {
+            document.getElementById('messageError').style.display = "none";
+        }, 6000);
+    } else {
+        document.getElementById('messageError').style.display = "none";
+        document.getElementById('messageOK').style.display = "block";
+        setTimeout(
+                function() {
+                    XmlHttp = GetXmlHttpObject();
+                    if (XmlHttp == null) {
+                        alert("Objets HTTP non supportés");
+                    } else {
+                        XmlHttp.onreadystatechange = stateChangedInscription;
+                        XmlHttp.open("GET", "./Controller/ReponsesAjax.php?action=inscri&pwd=" + pass + "&log=" + acc + "&email=" + email, false);
+                        XmlHttp.send(null);
+                    }
+                }, 2000);
+    }
+}
+
+function stateChangedInscription()
+{
+    if (XmlHttp.readyState == 4) {
+        json = JSON.parse(XmlHttp.responseText);
+        if (json.inscri) {
+            //avant d'appeller la méthode pour connecter l'utilisateur, je nouris les champs textes dont il a besoin !
+            document.getElementById("login").value = document.getElementById("log").value;
+            document.getElementById("password").value = document.getElementById("pwd").value;
+            validerConnexion()
+        } else {
+            alert("L'inscription ne peut pas avoir lieu.");
         }
     }
 }
