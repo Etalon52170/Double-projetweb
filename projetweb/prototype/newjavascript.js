0fb5473c12341c08a96c9c1c839c96f517657f08
nataris88 <nataris88@live.fr>
2014-11-28 15:12:23 +0100

    Icone centre + debut JS pour selectionner le signe

--- projetweb/prototype/newjavascript.js
+++ projetweb/prototype/newjavascript.js
@@ -1,23 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function addvanish(){
+	$("section").append(" <div class='vanish'></div>");
+	$('div.vanish').mouseover(hoverDiv);
+	$('div.vanish').click(color);
+}
 
-function hoverSpan(){
-	$(this).css("border-radius"," = 50%");
-        $(this).css("border-width"," = 5px");
-        $(this).css("border-style"," = solid");
-        $(this).css("border-color"," = Orange");
+function hoverDiv(){
+	$(this).css("height","+=10");
 }
 
 function switch1(){
 	$('div.vanish').slideToggle(600);
 } 
 
+function color(){
+	var colors = ['#ff0000', '#00ff00', '#0000ff','#222222','orange'];
+	var random_color = colors[Math.floor(Math.random() * colors.length)];
+	$(this).css("backgroundColor", random_color);
+}
+
+
 
 $(document).ready( function(){
-	$('span.Icone>').mouseover(hoverSpan);
-       // $('span.Icone').mouseleave(LeaveSpan);
+	//$('div.vanish').mouseover(hoverDiv);
+	//$('div.vanish').click(color);
+	//$('button#hide').click(switch1);
+	//$('button#add').click(addvanish);
 });
 
