491290df8a4ce617d95bedcc68da769711b1cf4d
nataris88 <nataris88@live.fr>
2014-11-29 15:21:57 +0100

    test envoie id ilustration en ajax + reflexion sur systeme php/Ajax

--- projetweb/prototype/newjavascript.js
+++ projetweb/prototype/newjavascript.js
@@ -1,28 +1,41 @@
-function addvanish(){
-	$("section").append(" <div class='vanish'></div>");
-	$('div.vanish').mouseover(hoverDiv);
-	$('div.vanish').click(color);
-}
+
 
 function hoverDiv(){
-	$(this).css("height","+=10");
+        $(this).css("border-color","blue");
+}
+function leaveDiv(){
+	$(this).css("border-color","#FFFFFF");
 }
 
-function switch1(){
-	$('div.vanish').slideToggle(600);
-} 
+function SendInfo()
+{
+    var value = $(this).attr('id'); // récupére l'attribue de l'icone séléctionner
+    console.log(value);
+    var my_data;
+    $.ajax({
+	url: "resto.php",
+	type: "GET",
+	data: { a : "value"},
+        
+    })
+    // console.log(data); ne fonctionne pas ici
+    /*.done(function( data ) {
+	my_data = data;
+	$.each(my_data, function(key, val) {
+            console.log(val); 
+            var message = $//('<section class="theme" data-id="'+val.id+'"><img src="'+val.imageUri+'" alt="logo"><div class="resto"><p>'+val.nom+'</p><a href="#"> Voir la carte</a></div></section>');
 
-function color(){
-	var colors = ['#ff0000', '#00ff00', '#0000ff','#222222','orange'];
-	var random_color = colors[Math.floor(Math.random() * colors.length)];
-	$(this).css("backgroundColor", random_color);
+            ('<div id="container_block"><div id="index_'+val.nom+'" class="blockIndex"><h2 class="title-blockIndex">'+val.nom+'</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet sed lacus ac aliquam. Aliquam a consequat nulla. Mauris ullamcorper, sem sit amet aliquam convallis, dolor mauris dignissim urna, eu iaculis nibh odio sit amet ligula.</p><a href="pizza.php" class="btn_blockIndex plat">Voir les plats</a></div></div>');
+            $("#container").append(message); 
+            message.click(); 
+        });
+    });*/
 }
-
-
 
 $(document).ready( function(){
-	//$('div.vanish').mouseover(hoverDiv);
-	//$('div.vanish').click(color);
+	$('p.Cperso').mouseover(hoverDiv);
+        $('p.Cperso').mouseleave(leaveDiv);
+	$('p.Cperso').click(SendInfo);
 	//$('button#hide').click(switch1);
 	//$('button#add').click(addvanish);
 });
