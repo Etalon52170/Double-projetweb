d9de927229dc9ee1cd51ee9ddf7351a61248357e
sopretty <rpzle54230@hotmailfr>
2014-11-30 16:29:50 +0100

    Ajout des méthodes de retour lors du lobby d'attente, de plusieurs méthodes ajax qui permettent l'actualisation du lobby d'attente

--- projetweb/JS/Js_Jeu.js
+++ projetweb/JS/Js_Jeu.js
@@ -1,4 +1,3 @@
-
 function deconnection() {
     XmlHttp = GetXmlHttpObject();
     if (XmlHttp == null) {
@@ -28,13 +27,88 @@ function GetXmlHttpObject()
 
 function stateChanged()
 {
-    //var json;
+    var json;
     if (XmlHttp.readyState == 4) {
         json = JSON.parse(XmlHttp.responseText);
         if (json.deco) {
             window.location.href = "./index.php";
         } else {
             alert("La deconnexion n'est pas possible !")
+        }
+    }
+}
+
+function newGame() {
+    location.href = "./index.php?a=partie";
+}
+
+function joinGame(id) {
+    location.href = "./index.php?a=partie&g=" + id;
+}
+
+var nbplayers;
+function actualiserJoueurs(nbjoueurs) {
+    nbplayers = nbjoueurs;
+    XmlHttp = GetXmlHttpObject();
+    var param = "action=actuJ&nbPlayers=" + nbjoueurs;
+    if (XmlHttp == null) {
+        alert("Objets HTTP non supportés");
+    } else {
+        XmlHttp.onreadystatechange = stateChangedActualiserJoueurs;
+        XmlHttp.open("POST", "./Controller/ReponsesAjax.php", false);
+        XmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        XmlHttp.send(param);
+    }
+}
+
+function stateChangedActualiserJoueurs()
+{
+    var json;
+    if (XmlHttp.readyState == 4) {
+        json = XmlHttp.responseText;
+        if (json != "") {
+            json = JSON.parse(XmlHttp.responseText);
+            if (json.nbJoueurs < 4) {
+                window.document.getElementById("barre").innerHTML = json.code;
+                nbplayers = json.nbJoueurs;
+                setTimeout(function() {
+                    actualiserJoueurs(nbplayers);
+                }, 1000);
+            } else {
+                window.location.href = "./index.php?a=jeux";
+            }
+        } else {
+            setTimeout(function() {
+                actualiserJoueurs(nbplayers);
+            }, 1000);
+        }
+    }
+}
+
+function versJeux() {
+    location.href = "./index.php?a=jeux";
+}
+
+function retour() {
+    XmlHttp = GetXmlHttpObject();
+    var param = "action=decPlay";
+    if (XmlHttp == null) {
+        alert("Objets HTTP non supportés");
+    } else {
+        XmlHttp.onreadystatechange = stateChangedDecrementerJoueurs;
+        XmlHttp.open("POST", "./Controller/ReponsesAjax.php", false);
+        XmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        XmlHttp.send(param);
+    }
+}
+
+function stateChangedDecrementerJoueurs()
+{
+    var json;
+    if (XmlHttp.readyState == 4) {
+        json = JSON.parse(XmlHttp.responseText);
+        if (json.decrementer == "ok") {
+            location.href = "./index.php?a=jeux";
         }
     }
 }
