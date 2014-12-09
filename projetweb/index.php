a51db433219e5653c58f4e348276141a4d804da6
sopretty <rpzle54230@hotmailfr>
2014-12-09 16:41:14 +0100

    Ajout du code permettant la création des cartes si elles ne le sont pas

--- projetweb/index.php
+++ projetweb/index.php
@@ -4,51 +4,56 @@ if (empty($_SESSION)) {
     session_start();
 }
 
-include './Controller/WebController.php';
+include_once './Controller/WebController.php';
+include_once './Model/cards.php';
 $controleur = new WebController();
 $controleur->callAction($_GET);
-/*
-  include './Model/cards.php';
-  $a = cards();
-  // de 0 à 56
-  foreach ($a as $idCards => $arrayIcon) {
-  $cards = new cards();
-  $cards->symbol0 = $arrayIcon[0];
-  $cards->symbol1 = $arrayIcon[1];
-  $cards->symbol2 = $arrayIcon[2];
-  $cards->symbol3 = $arrayIcon[3];
-  $cards->symbol4 = $arrayIcon[4];
-  $cards->symbol5 = $arrayIcon[5];
-  $cards->symbol6 = $arrayIcon[6];
-  $cards->symbol7 = $arrayIcon[7];
-  $cards->insert();
-  }
-  function cards() {
+$list_card = cards::findAll();
+if (empty($list_card)) {
+    $a = cards();
+    // de 0 à 56
+    foreach ($a as $idCards => $arrayIcon) {
+        $cards = new cards();
+        $cards->symbol0 = $arrayIcon[0];
+        $cards->symbol1 = $arrayIcon[1];
+        $cards->symbol2 = $arrayIcon[2];
+        $cards->symbol3 = $arrayIcon[3];
+        $cards->symbol4 = $arrayIcon[4];
+        $cards->symbol5 = $arrayIcon[5];
+        $cards->symbol6 = $arrayIcon[6];
+        $cards->symbol7 = $arrayIcon[7];
+        $cards->insert();
+    }
 
-  function c($i, $j) {
-  return $i + 7 * $j + 9;
-  }
 
-  $A = array();
-  $B = array();
-  $C = array();
-  for ($i = 0; $i < 8; $i++) {
-  $A[0][] = 1 + $i;
-  }
-  for ($i = 0; $i < 7; $i++) {
-  $B[$i][] = 1;
-  for ($j = 0; $j < 7; $j++) {
-  $B[$i][] = c($i, $j);
-  }
-  }
-  for ($i = 0; $i < 7; $i++) {
-  for ($j = 0; $j < 7; $j++) {
-  $C[$i + 7 * $j][] = $i + 2;
-  for ($k = 0; $k < 7; $k++) {
-  $C[$i + 7 * $j][] = c($k, ($k * $i + $j) % 7);
-  }
-  }
-  }
-  return array_merge($A, $B, $C);
-  } */
+}
+function cards() {
+
+    function c($i, $j) {
+        return $i + 7 * $j + 9;
+    }
+
+    $A = array();
+    $B = array();
+    $C = array();
+    for ($i = 0; $i < 8; $i++) {
+        $A[0][] = 1 + $i;
+    }
+    for ($i = 0; $i < 7; $i++) {
+        $B[$i][] = 1;
+        for ($j = 0; $j < 7; $j++) {
+            $B[$i][] = c($i, $j);
+        }
+    }
+    for ($i = 0; $i < 7; $i++) {
+        for ($j = 0; $j < 7; $j++) {
+            $C[$i + 7 * $j][] = $i + 2;
+            for ($k = 0; $k < 7; $k++) {
+                $C[$i + 7 * $j][] = c($k, ($k * $i + $j) % 7);
+            }
+        }
+    }
+    return array_merge($A, $B, $C);
+}
+
 ?>
